83a5648b5a56d965d0109d2f110b2c16f121cf67
Tobias S <tobias.seifert97@gmail.com>
2023-09-09 14:52:53 +0200

    feat(chat)!: rewrite chat message logic

--- lua/flyboy/openai.lua
+++ lua/flyboy/openai.lua
@@ -1,7 +1,39 @@
-local curl = require('plenary.curl')
+local function recieve_chunk(chunk, on_stdout_chunk)
+    for line in chunk:gmatch("[^\n]+") do
+        local raw_json = string.gsub(line, "^data: ", "")
 
-local function get_chatgpt_completion(options, messages, on_delta, on_complete)
-    curl.post(options.url,
+        local ok, path = pcall(vim.json.decode, raw_json)
+        if not ok then
+            on_stdout_chunk(raw_json, path)
+            goto continue
+        end
+
+        path = path.choices
+        if path == nil then
+            goto continue
+        end
+        path = path[1]
+        if path == nil then
+            goto continue
+        end
+        path = path.delta
+        if path == nil then
+            goto continue
+        end
+        path = path.content
+        if path == nil then
+            goto continue
+        end
+
+        on_stdout_chunk(path)
+        -- append_to_output(path, 0)
+        ::continue::
+    end
+end
+
+local curl = require('plenary.curl')
+local function get_chatgpt_completion(options, messages, on_delta)
+    return curl.post(options.url,
         {
             headers = options.headers,
             body = vim.fn.json_encode(
@@ -11,19 +43,13 @@ local function get_chatgpt_completion(options, messages, on_delta, on_complete)
                     messages = messages,
                     stream = true
                 }),
-            stream = vim.schedule_wrap(
-                function(_, data, _)
-                    local raw_message = string.gsub(data, "^data: ", "")
-                    if raw_message == "[DONE]" then
-                        on_complete()
-                    elseif (string.len(data) > 6) then
-                        on_delta(vim.fn.json_decode(string.sub(data, 6)))
-                    end
-                end)
+            stream = vim.schedule_wrap(function(_, data, _)
+                recieve_chunk(data, on_delta)
+            end),
         })
 end
 
 
 return {
-    get_chatgpt_completion = get_chatgpt_completion
+    get_chatgpt_completion = get_chatgpt_completion,
 }

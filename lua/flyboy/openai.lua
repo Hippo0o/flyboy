659c5f678a903925636d98e3eba49b932da895e7
Tobias S <seifert@devlabor.com>
2023-09-13 16:30:47 +0200

    feat: better error handling + cancel keybind

--- lua/flyboy/openai.lua
+++ lua/flyboy/openai.lua
@@ -1,10 +1,10 @@
-local function recieve_chunk(chunk, on_stdout_chunk)
+local function recieve_chunk(chunk, on_stdout_chunk, on_chunk_error)
     for line in chunk:gmatch("[^\n]+") do
         local raw_json = string.gsub(line, "^data: ", "")
 
         local ok, path = pcall(vim.json.decode, raw_json)
         if not ok then
-            on_stdout_chunk(raw_json, path)
+            on_chunk_error(raw_json)
             goto continue
         end
 
@@ -32,8 +32,19 @@ local function recieve_chunk(chunk, on_stdout_chunk)
 end
 
 local curl = require('plenary.curl')
-local function get_chatgpt_completion(options, messages, on_delta)
-    return curl.post(options.url,
+local function get_chatgpt_completion(options, messages, on_delta, on_error_cb)
+    local had_error = false
+    local job
+
+    local function on_error(err)
+        if on_error_cb and not had_error then
+            on_error_cb(err)
+        end
+        job:shutdown()
+        had_error = true
+    end
+
+    job = curl.post(options.url,
         {
             headers = options.headers,
             body = vim.fn.json_encode(
@@ -44,9 +55,15 @@ local function get_chatgpt_completion(options, messages, on_delta)
                     stream = true
                 }),
             stream = vim.schedule_wrap(function(_, data, _)
-                recieve_chunk(data, on_delta)
+                local ok, err = pcall(recieve_chunk, data, on_delta, on_error)
+                if not ok then
+                    on_error(err)
+                end
             end),
+            on_error = on_error,
         })
+
+    return job
 end
 
 
